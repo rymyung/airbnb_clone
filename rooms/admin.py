101e8c3dfa8a4e92bc97b7415f9f733907cf742a
rymyung <rymyung@hanmail.net>
2023-07-28 18:04:31 +0900

    feat: room 모델과 amenity 모델의 관리자 페이지를 수정한다

--- rooms/admin.py
+++ rooms/admin.py
@@ -5,8 +5,28 @@ from .models import Amenity, Room
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "name",
+        "price",
+        "kind",
+        "owner",
+    )
+
+    list_filter = (
+        "country",
+        "city",
+        "price",
+        "rooms",
+        "toilets",
+        "pet_friendly",
+        "amenities",
+    )
 
 @admin.register(Amenity)
 class AmenityAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "name",
+        "description",
+        "created_at",
+        "updated_at",
+    )
