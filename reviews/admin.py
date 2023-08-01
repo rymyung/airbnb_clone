e3cb9e65e08bcffc54277887bbe4227d13c8ade8
rymyung <rymyung@hanmail.net>
2023-08-01 22:34:14 +0900

    feat(reviews/admin.py): reivew model 관리자 페이지에 custom filter를 추가한다

--- reviews/admin.py
+++ reviews/admin.py
@@ -3,6 +3,27 @@ from django.contrib import admin
 from .models import Review
 
 
+class WordFilter(admin.SimpleListFilter):
+
+    title = "Filter by words!"
+
+    parameter_name = "filtering_word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("great", "Great"),
+            ("good", "Good"),
+            ("awesome", "Awesome"),
+        ]
+
+    def queryset(self, request, reviews):
+        filtering_word = self.value()
+        if filtering_word:
+            return reviews.filter(payload__contains=filtering_word)
+        else:
+            return reviews
+
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
@@ -11,5 +32,8 @@ class ReviewAdmin(admin.ModelAdmin):
     )
 
     list_filter = (
+        WordFilter,
         "rating",
+        "user__is_host", # FK's field
+        "room__category", # FK's field
     )
