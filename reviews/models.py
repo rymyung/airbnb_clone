110217662721e107bcfcb97a44df3fdad0946eb8
rymyung <rymyung@hanmail.net>
2023-08-01 21:47:14 +0900

    feat(review/models.py): review model에 related_name을 설정한다

--- reviews/models.py
+++ reviews/models.py
@@ -5,9 +5,15 @@ from common.models import CommonModel
 
 class Review(CommonModel):
     """Review from a User to a Room or Experience"""
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE, null=True, blank=True)
-    experience = models.ForeignKey("experiences.Experience", on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="reviews")
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE, null=True, blank=True, related_name="reviews")
+    experience = models.ForeignKey(
+        "experiences.Experience",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="reviews",
+    )
 
     payload = models.TextField()
     rating = models.PositiveIntegerField()
