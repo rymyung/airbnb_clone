5021802b9c53e503b2f5a36426d113ed9b42c43e
rymyung <rymyung@hanmail.net>
2023-08-02 22:44:31 +0900

    feat: rooms app의 urld과 view를 생성한다

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,34 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-# Create your views here.
+from .models import Room
+
+
+def see_all_rooms(request):
+    rooms = Room.objects.all()
+    return render(
+        request,
+        "all_rooms.html",
+        {
+            "rooms": rooms,
+            "title": "Hello! This title comes from django!"
+        }
+    )
+
+def see_one_room(request, room_pk):
+    try:
+        room = Room.objects.get(pk=room_pk)
+        return render(
+            request,
+            "room_detail.html",
+            {
+                "room": room,
+            }
+        )
+    except Room.DoesNotExist:
+        return render(
+            request,
+            "room_detail.html",
+            {
+                "not_found": True,
+            }
+        )
