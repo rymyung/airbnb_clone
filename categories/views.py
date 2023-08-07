12698bf1183b3d34a8c8f157225ae555eff15571
rymyung <rymyung@hanmail.net>
2023-08-07 15:29:53 +0900

    refactor: ModelViewSet으로 변경한다

--- categories/views.py
+++ categories/views.py
@@ -1,58 +1,10 @@
-from rest_framework.exceptions import NotFound
-from rest_framework.response import Response
-from rest_framework.status import HTTP_204_NO_CONTENT
-from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Category
 from .serializers import CategorySerializer
 
 
-class Categories(APIView):
+class CategoryViewSet(ModelViewSet):
 
-    def get(self, request):
-        all_categories = Category.objects.all()
-        serializer = CategorySerializer(all_categories, many=True)
-        return Response(
-            {
-                "ok": True,
-                "categories": serializer.data,
-            }
-        )
-
-    def post(self, request):
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            new_category = serializer.save()
-            return Response(CategorySerializer(new_category).data)
-        else:
-            return Response(serializer.errors)
-
-
-class CategoryDetail(APIView):
-
-    def get_object(self, pk):
-        try:
-            return Category.objects.get(pk=pk)
-        except Category.DoesNotExist:
-            raise NotFound
-
-    def get(self, request, pk):
-        serializer = CategorySerializer(self.get_object(pk))
-        return Response(
-            {
-                "ok": True,
-                "category": serializer.data,
-            }
-        )
-
-    def put(self, request, pk):
-        serializer = CategorySerializer(self.get_object(pk), data=request.data, partial=True)
-        if serializer.is_valid():
-            updated_category = serializer.save()
-            return Response(CategorySerializer(updated_category).data)
-        else:
-            return Response(serializer.errors)
-
-    def delete(self, request, pk):
-        self.get_object(pk).delete()
-        return Response(status=HTTP_204_NO_CONTENT)
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
