be30b00c352cfade9719486d90391e8ffdeda015
rymyung <rymyung@hanmail.net>
2023-08-09 16:29:33 +0900

    feat: user api 개발

--- users/serializers.py
+++ users/serializers.py
@@ -7,3 +7,22 @@ class TinyUserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ("name", "avatar", "username")
+
+
+class PrivateUserSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = (
+            "pk", "name", "username", "first_name", "last_name", "email",
+            "gender", "language", "currency", "is_host"
+        )
+        # exclude = ("password")
+
+
+# TODO: 갖고있는 house 수, 작성한 리뷰 보여주기
+class PublicUserSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = "__all__"
