f9528d1b0884b805baf9b5a30a431587d024756a
rymyung <rymyung@hanmail.net>
2023-08-15 12:53:12 +0900

    feat: Token Authentication을 생성한다

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 THIRD_PARTY_APPS = [
     "rest_framework",
+    "rest_framework.authtoken", # Token AUthentication
 ]
 
 CUSTOM_APPS = [
@@ -148,3 +149,11 @@ MEDIA_ROOT = "uploads"
 MEDIA_URL = "user-uploads/"
 
 PAGE_SIZE = 2
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        # "config.authentication.TrustMeBroAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
